d77bd55c9b7f20b8562d4b8779262c64dfc404a5
al223ec <al223ec@student.lnu.se>
2014-11-18 17:06:53 +0100

    Init och steg 1

---
+++ ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoUniversity.Models;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext 
+    {
+        //The name of the connection string is passed in to the constructor.
+        public SchoolContext()
+            : base("SchoolContext")
+        { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); 
+        }
+
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        /*
+         * Navigation properties are typically defined as virtual so that they can take advantage of certain Entity Framework functionality
+         * such as lazy loading. 
+         * If a navigation property can hold multiple entities (as in many-to-many or one-to-many relationships), its type must be a 
+         * list in which entries can be added, deleted, and updated, such as ICollection.
+         */
+    }
+}
